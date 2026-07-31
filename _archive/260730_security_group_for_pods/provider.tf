8c479054caeb6ec954d9081d6c228fa151ccd046
rainwnssystem <diaprvw@gmail.com>
2026-07-31 21:20:04 +0900

    feat(archive): k8s security groups for pods practice

---
+++ _archive/260730_security_group_for_pods/provider.tf
@@ -0,0 +1,90 @@
+#################################
+
+variable "project_name" {
+  default = "wsi"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+#################################
+
+terraform {
+  required_providers {
+    awsutils = {
+      source = "cloudposse/awsutils"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.19"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      project = var.project_name
+      owner   = "boseok"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
+
+provider "awsutils" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    command     = "aws"
+  }
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  apply_retry_count      = 15
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+      command     = "aws"
+    }
+  }
+}
+
+data "http" "myip" {
+  url = "https://myip.wtf/text"
+}
+
+data "aws_caller_identity" "caller" {
+
+}
+
+data "aws_ecrpublic_authorization_token" "token" {
+  provider = aws.us-east-1
+}
